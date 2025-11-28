d75e96bbbc1ccdda1fecce9c3a1bbb9807ca5a3a
a-palomino <76058124+a-palomino@users.noreply.github.com>
2025-11-28 12:35:45 +0100

    Primer commit
    Primera versión de prueba

---
+++ scripts/generatePDF.mjs
@@ -0,0 +1,64 @@
+//import { jsPDF } from "jspdf";
+
+window.addEventListener("DOMContentLoaded", (event)=>{
+    document.getElementById("btnEnviar").addEventListener("click", function(e){
+        e.preventDefault();
+        
+        let tipo = document.getElementById("tipoDoc").value;
+        let numFactura = document.getElementById("numFactura").value;
+        let fecha = document.getElementById("fecha").value;
+        let cliente = document.getElementById("cliente").value;
+        let dirCliente = document.getElementById("direccion").value;
+        let localidadCliente = document.getElementById("localidad").value;
+        let conceptos = "1";
+
+        //Hacer función para obtener los datos de los conceptos creados
+        generarPDF(tipo,numFactura,fecha,cliente,dirCliente,localidadCliente,conceptos);
+    });
+});
+
+
+const {jsPDF} = window.jspdf;
+const doc = new jsPDF();
+
+/**
+ * 
+ * @param {String} tipo 
+ * @param {String} numFactura
+ * @param {String} fecha 
+ * @param {String} cliente 
+ * @param {String} direccionCliente 
+ * @param {String} localidadCliente 
+ * @param {Array} conceptos 
+ */
+function generarPDF(tipo, numFactura, fecha,cliente,direccionCliente,localidadCliente, conceptos){
+    
+    let dni = "80081036G";
+    let dirEmpresa = "Dirección 06140 TALAVERA LA REAL (BADAJOZ)";
+    let posX = 0;
+    let posY = 0;
+
+    //Encabezado Doc
+    doc.setFontSize(30);
+    doc.text(tipo,19,20);
+    doc.setDrawColor(255, 0, 0);
+    doc.text("[" + numFactura + "]",40,20);
+
+    doc.setFontSize(25);
+    doc.text("RAIMUNDO SANTIAGO FERRERA",20,40);
+    doc.setDrawColor(0, 0, 0);
+    doc.setFontSize(12);
+    doc.text(dni,20,50);
+    doc.text(dirEmpresa,20,60);
+
+    doc.addImage("../media/", "JPEG",70,20);
+    doc.line(19,70,200,70);
+
+
+
+
+    let namePDF = tipo + numFactura;
+    doc.save(namePDF+ ".pdf");
+
+
+}
---
+++ scripts/index.mjs
@@ -0,0 +1,117 @@
+//Event listener botones
+
+window.addEventListener("DOMContentLoaded", (event)=>{
+    document.getElementById("btnAnadir").addEventListener("click", function(e){
+        e.preventDefault();
+        anadirConcepto();
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Funciones form
+
+/**
+ * Crea un componente nuevo de concepto y lo devuelve 
+ * @returns Node, Nuevo concepto dentro de un div
+ */
+function crearNodoConcepto(){
+    let nodeConceptos = document.getElementById("conceptos");
+    let divsConceptos = nodeConceptos.querySelectorAll("div");
+    let numConceptos = divsConceptos.length;
+
+    //names and id nodo
+    let descripcionNodo = "descripcion" + (numConceptos+1);
+    let cantidadNodo = "cantidad" + (numConceptos+1);
+    let precioNodo = "precio" + (numConceptos+1);
+    let btnID = "eliminar" + (numConceptos+1);
+    let nodoID = "nodo" + (numConceptos+1);
+
+    //Creamos un nodo
+
+    //Descripción
+    let labelDescripcion = document.createElement("label");
+    labelDescripcion.setAttribute("for",descripcionNodo);
+    let labelDescripcionText = document.createTextNode("Descripción");
+    labelDescripcion.appendChild(labelDescripcionText);
+
+    let inputDescripcion = document.createElement("input");
+    inputDescripcion.setAttribute("type", "text");
+    inputDescripcion.setAttribute("name", descripcionNodo);
+    inputDescripcion.setAttribute("id", descripcionNodo);
+
+    //Cantidad
+    let labelCantidad = document.createElement("label");
+    labelCantidad.setAttribute("for",cantidadNodo);
+    let labelCantidadText = document.createTextNode("Cantidad");
+    labelCantidad.appendChild(labelCantidadText);
+
+    let inputCantidad = document.createElement("input");
+    inputCantidad.setAttribute("type", "number");
+    inputCantidad.setAttribute("name", cantidadNodo);
+    inputCantidad.setAttribute("id", cantidadNodo);
+
+    //Precio unidad
+    let labelPrecio = document.createElement("label");
+    labelPrecio.setAttribute("for",precioNodo);
+    let labelPrecioText = document.createTextNode("Precio por unidad");
+    labelPrecio.appendChild(labelPrecioText);
+
+    let inputPrecio = document.createElement("input");
+    inputPrecio.setAttribute("type", "number");
+    inputPrecio.setAttribute("name", precioNodo);
+    inputPrecio.setAttribute("id", precioNodo);
+
+    //Btn remove
+    let btnEliminar = document.createElement("button");
+    let btnTexto = document.createTextNode("Eliminar");
+    btnEliminar.setAttribute("class","btn btn-danger");
+    btnEliminar.setAttribute("id", btnID);
+    btnEliminar.appendChild(btnTexto);
+    btnEliminar.addEventListener("click", function(e){
+        let parentDiv = document.getElementById(nodoID);
+        parentDiv.remove();
+    });
+    //Div
+    let divNodo = document.createElement("div");
+    divNodo.setAttribute("class", "mb-3");
+    divNodo.setAttribute("id", nodoID);
+
+    divNodo.appendChild(labelDescripcion);
+    divNodo.appendChild(inputDescripcion);
+    divNodo.appendChild(labelCantidad);
+    divNodo.appendChild(inputCantidad);
+    divNodo.appendChild(labelPrecio);
+    divNodo.appendChild(inputPrecio);
+    divNodo.appendChild(btnEliminar);
+
+
+    return divNodo;
+}
+
+
+function anadirConcepto(){
+    
+
+    
+
+    let nodeConceptos = document.getElementById("conceptos");
+    let btnAnadir = document.getElementById("btnAnadir");
+    let nuevoConcepto = crearNodoConcepto();
+
+    nodeConceptos.insertBefore(nuevoConcepto,btnAnadir);
+    //nodeConceptos.appendChild(nuevoConcepto);
+
+    
+}
